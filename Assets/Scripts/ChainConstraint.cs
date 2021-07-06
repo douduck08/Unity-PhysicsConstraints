bb658212c0f3875e24807626544b26ba7ff63aa5
douduck08 <lu10082000@gmail.com>
2021-07-06 18:27:26 +0800

    trying chain constraint

---
+++ Assets/Scripts/ChainConstraint.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+
+public class ChainConstraint : MonoBehaviour {
+
+    [System.Serializable]
+    public struct ConstraintConfig {
+        public Transform deformedObject;
+        public float3 offset;
+    }
+
+    public struct ConstraintData {
+        public float mass;
+        public float massInv;
+        public float3x3 inertia;
+        public float3x3 inertiaInv;
+        public float3 linarVelocity;
+        public float3 angularVelocity;
+    }
+
+    [SerializeField] Transform targetPoint;
+    [SerializeField] ConstraintConfig[] constraints;
+    ConstraintData[] constraintDatas;
+
+    [Header ("Global Config")]
+    [SerializeField] float3 extraForce = new float3 (0, -9.8f, 0);
+    [SerializeField, Range (0f, 1f)] float beta = 1f;
+    [SerializeField, Range (0f, 1f)] float damping = 0.999f;
+
+
+    void Start () {
+        constraintDatas = new ConstraintData[constraints.Length];
+
+        var mass = 1f;
+        var size = 1f;
+        for (int i = 0; i < constraints.Length; i++) {
+            constraintDatas[i].mass = mass;
+            constraintDatas[i].massInv = 1f / mass;
+            constraintDatas[i].inertia = (mass * size * size / 6f) * float3x3.identity; // Inertia Tensor for a Cube
+            constraintDatas[i].inertiaInv = math.inverse (constraintDatas[i].inertia);
+        }
+    }
+
+    void FixedUpdate () {
+        var dt = Time.fixedDeltaTime;
+        UpdateConstraint (Time.fixedDeltaTime);
+
+        for (int i = 0; i < constraints.Length; i++) {
+            var deformedObject = constraints[i].deformedObject;
+            deformedObject.position += (Vector3)constraintDatas[i].linarVelocity * dt;
+            deformedObject.rotation = Quaternion.Euler (constraintDatas[i].angularVelocity * dt * Mathf.Rad2Deg) * deformedObject.rotation;
+        }
+    }
+
+    void UpdateConstraint (float dt) {
+        for (int i = 0; i < constraints.Length; i++) {
+            // apply extra forces
+            constraintDatas[i].linarVelocity = constraintDatas[i].linarVelocity + extraForce * dt;
+        }
+
+        for (int it = 0; it < 10; it++) {
+            for (int i = 0; i < constraints.Length; i++) {
+                var deformedObject = constraints[i].deformedObject;
+                float3 targetPos = (i == 0) ? targetPoint.position : constraints[i - 1].deformedObject.position;
+                float3 position = deformedObject.position;
+                float3 r = deformedObject.rotation * constraints[i].offset;
+
+                // caculate constraints
+                float3 linarVelocity = constraintDatas[i].linarVelocity;
+                float3 angularVelocity = constraintDatas[i].angularVelocity;
+                float3 positionConstraint = (position + r) - targetPos;
+                float3 velocityConstraint = linarVelocity + math.cross (angularVelocity, r);
+                if (i > 0) {
+                    velocityConstraint -= constraintDatas[i - 1].linarVelocity;
+                }
+
+                // transform inertia tensor
+                float3x3 localToWorld = new float3x3
+                (
+                    deformedObject.localToWorldMatrix.m00, deformedObject.localToWorldMatrix.m01, deformedObject.localToWorldMatrix.m02,
+                    deformedObject.localToWorldMatrix.m10, deformedObject.localToWorldMatrix.m11, deformedObject.localToWorldMatrix.m12,
+                    deformedObject.localToWorldMatrix.m20, deformedObject.localToWorldMatrix.m21, deformedObject.localToWorldMatrix.m22
+                );
+                float3x3 inertiaInvWs = math.mul (localToWorld, math.mul (constraintDatas[i].inertiaInv, math.transpose (localToWorld)));
+
+                // solve constraints
+                float3x3 s = MathUtils.SkewSymmetric (-r); // matrix for cross dot
+                float3x3 st = math.transpose (s);
+                float3x3 k = (constraintDatas[i].massInv * float3x3.identity) + math.mul (s, math.mul (inertiaInvWs, st));
+                if (i > 0) {
+                    k = (constraintDatas[i - 1].massInv * float3x3.identity) + k;
+                }
+                float3 bias = (beta / dt) * positionConstraint; // bias velocity vector
+                float3 lambda = math.mul (math.inverse (k), -(velocityConstraint + bias)); // = effectiveMass * -(JV + b)
+
+                // add constraint forces
+                linarVelocity += constraintDatas[i].massInv * lambda;
+                angularVelocity += math.mul (inertiaInvWs, math.mul (st, lambda));
+                constraintDatas[i].linarVelocity = linarVelocity * damping;
+                constraintDatas[i].angularVelocity = angularVelocity * damping;
+            }
+        }
+    }
+}
--- Assets/Scripts/PointConstraint.cs
+++ Assets/Scripts/PointConstraint.cs
@@ -21,69 +21,64 @@ public class PointConstraint : MonoBehaviour {
         public float3 angularVelocity;
     }
 
-    [SerializeField] ConstraintConfig[] constraints;
+    [SerializeField] ConstraintConfig constraint;
+    ConstraintData constraintData;
 
     [Header ("Global Config")]
     [SerializeField] float3 extraForce = new float3 (0, -9.8f, 0);
     [SerializeField, Range (0f, 1f)] float beta = 1f;
-    [SerializeField, Range (0f, 1f)] float damping = 0.99f;
-
-    ConstraintData[] constraintDatas;
+    [SerializeField, Range (0f, 1f)] float damping = 0.999f;
 
     void Start () {
-        constraintDatas = new ConstraintData[constraints.Length];
-
         var mass = 1f;
         var size = 1f;
-        for (int i = 0; i < constraints.Length; i++) {
-            constraintDatas[i].mass = mass;
-            constraintDatas[i].massInv = 1f / mass;
-            constraintDatas[i].inertia = (mass * size * size / 6f) * float3x3.identity; // Inertia Tensor for a Cube
-            constraintDatas[i].inertiaInv = math.inverse (constraintDatas[i].inertia);
-        }
+        constraintData.mass = mass;
+        constraintData.massInv = 1f / mass;
+        constraintData.inertia = (mass * size * size / 6f) * float3x3.identity; // Inertia Tensor for a Cube
+        constraintData.inertiaInv = math.inverse (constraintData.inertia);
     }
 
     void FixedUpdate () {
-        UpdatePerConstraint (Time.fixedDeltaTime);
+        var dt = Time.fixedDeltaTime;
+        UpdateConstraint (dt);
+
+        var deformedObject = constraint.deformedObject;
+        deformedObject.position += (Vector3)constraintData.linarVelocity * dt;
+        deformedObject.rotation = Quaternion.Euler (constraintData.angularVelocity * dt * Mathf.Rad2Deg) * deformedObject.rotation;
     }
 
-    void UpdatePerConstraint (float dt) {
-        for (int i = 0; i < constraints.Length; i++) {
-            var deformedObject = constraints[i].deformedObject;
-            float3 targetPos = constraints[i].targetPoint.position;
-            float3 position = deformedObject.position;
-            float3 r = deformedObject.rotation * constraints[i].offset;
+    void UpdateConstraint (float dt) {
+        var deformedObject = constraint.deformedObject;
+        float3 targetPos = constraint.targetPoint.position;
+        float3 position = deformedObject.position;
+        float3 r = deformedObject.rotation * constraint.offset;
 
-            float3 linarVelocity = constraintDatas[i].linarVelocity + extraForce * dt;
-            float3 angularVelocity = constraintDatas[i].angularVelocity;
-            float3 positionConstraint = (position + r) - targetPos;
-            float3 velocityConstraint = linarVelocity + math.cross (angularVelocity, r);
+        // apply extra forces and caculate constraints
+        float3 linarVelocity = constraintData.linarVelocity + extraForce * dt;
+        float3 angularVelocity = constraintData.angularVelocity;
+        float3 positionConstraint = (position + r) - targetPos;
+        float3 velocityConstraint = linarVelocity + math.cross (angularVelocity, r);
 
-            float3x3 localToWorld = new float3x3
-            (
-                deformedObject.localToWorldMatrix.m00, deformedObject.localToWorldMatrix.m01, deformedObject.localToWorldMatrix.m02,
-                deformedObject.localToWorldMatrix.m10, deformedObject.localToWorldMatrix.m11, deformedObject.localToWorldMatrix.m12,
-                deformedObject.localToWorldMatrix.m20, deformedObject.localToWorldMatrix.m21, deformedObject.localToWorldMatrix.m22
-            );
-            float3x3 inertiaInvWs = math.mul (localToWorld, math.mul (constraintDatas[i].inertiaInv, math.transpose (localToWorld)));
+        // transform inertia tensor
+        float3x3 localToWorld = new float3x3
+        (
+            deformedObject.localToWorldMatrix.m00, deformedObject.localToWorldMatrix.m01, deformedObject.localToWorldMatrix.m02,
+            deformedObject.localToWorldMatrix.m10, deformedObject.localToWorldMatrix.m11, deformedObject.localToWorldMatrix.m12,
+            deformedObject.localToWorldMatrix.m20, deformedObject.localToWorldMatrix.m21, deformedObject.localToWorldMatrix.m22
+        );
+        float3x3 inertiaInvWS = math.mul (localToWorld, math.mul (constraintData.inertiaInv, math.transpose (localToWorld)));
 
-            float3x3 sr = MathUtils.SkewSymmetric (-r); // for cross dot
-            float3x3 sr_t = math.transpose (sr);
-            float3x3 k = (constraintDatas[i].massInv * float3x3.identity) + math.mul (sr, math.mul (inertiaInvWs, sr_t));
-            float3x3 effectiveMass = math.inverse (k);
-            float3 b = (beta / dt) * positionConstraint; // bias velocity vector
-            float3 lambda = math.mul (effectiveMass, -(velocityConstraint + b)); // = -1 * effectiveMass * (JV + b)
+        // solve constraints
+        float3x3 s = MathUtils.SkewSymmetric (-r); // matrix for cross dot
+        float3x3 st = math.transpose (s);
+        float3x3 k = (constraintData.massInv * float3x3.identity) + math.mul (s, math.mul (inertiaInvWS, st));
+        float3 bias = (beta / dt) * positionConstraint; // bias velocity vector
+        float3 lambda = math.mul (math.inverse (k), -(velocityConstraint + bias)); // = effectiveMass * -(JV + b)
 
-            linarVelocity += constraintDatas[i].massInv * lambda;
-            angularVelocity += math.mul (inertiaInvWs, math.mul (sr_t, lambda));
-            linarVelocity *= damping;
-            angularVelocity *= damping;
-
-            // integration and update
-            deformedObject.position += (Vector3)linarVelocity * dt;
-            deformedObject.rotation = Quaternion.Euler (angularVelocity * dt * Mathf.Rad2Deg) * deformedObject.rotation;
-            constraintDatas[i].linarVelocity = linarVelocity;
-            constraintDatas[i].angularVelocity = angularVelocity;
-        }
+        // add constraint forces
+        linarVelocity += constraintData.massInv * lambda;
+        angularVelocity += math.mul (inertiaInvWS, math.mul (st, lambda));
+        constraintData.linarVelocity = linarVelocity * damping;
+        constraintData.angularVelocity = angularVelocity * damping;
     }
 }
